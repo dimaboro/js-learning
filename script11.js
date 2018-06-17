78ffbfd575b7b92aa70213ddbd5b003209370cb7
dimaboro <dimaboro2001@gmail.com>
2018-06-17 22:00:40 +0300

    11

---
+++ script11.js
@@ -0,0 +1,97 @@
+// свойство .length - выводит длину введенной строки
+
+alert("Hello, world".length); //12
+
+// метод .toUpperCase() - возвращает строку в верхнем регистре
+
+alert( "hello, world".toUpperCase() ); // HELLO, WORLD
+
+// метод .toFixed(n) - возвращает число с n знаков после запятой
+
+var n = 12.345
+
+console.log( n.toFixed(5) ); // 12.34500
+
+console.log( 12..toFixed(7) ); //чтобы работало, надо точку после числа
+
+console.log( 0xFF ); //255 в 16 сс
+
+console.log( 5e6 ); // 5 000 000
+
+console.log( 5 / 0 ); // infinity
+
+// infinity - особенное численное значение
+
+// если операция не может быть совершена, возвращается NaN - Not a Number
+
+console.log( 0 / 0 ); // NaN
+
+// NaN не равно ничему, включая себя
+
+if (NaN == NaN) console.log("==");
+
+if (NaN === NaN) console.log("===");
+
+// нифига не сработало
+
+// можно проверить через isNaN - только для NaN
+
+var n = 0 / 0;
+
+console.log( isNaN(n) ); //true
+
+// операции с NaN возвращают NaN
+
+if (n !== n) console.log("NaN"); // работает
+if ( isNaN(n) ) console.log("NaN"); // но этот способ лучше
+
+// isFinite(п) возвращает true, если п != NaN, infinity, -infinity
+
+console.log( isFinite(Infinity) ); //false
+
+var s = 123;
+
+console.log( +s ); //123
+
+console.log("12t"); //NaN
+
+// пробелы игнорируются, символ пробела \n или " "
+
+console.log(+""); // 0
+
+console.log(+"1 2"); // NaN
+
+console.log("33." / "-3"); //11
+
+// parseInt, parseFloat - вернет только число из строки
+// parseFloat - работает с драбными числами
+// до первой ошибки
+
+console.log( parseInt("12px") ); //12
+
+console.log( parseFloat("12.3456.7") ); //12.3456
+
+// в parseInt можно задавать систему счисления
+
+console.log( parseInt( "0xFF", 16 ) ); // 255
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n) ) && isFinite(n);
+}
+
+// toString делает из одной СС число в другой
+
+n = 255;
+
+console.log( n.toString(16) );
+
+// основание от 2 до 36
+
+var n = 123456789;
+
+console.log(n.toString(36) );
+
+// округление до целого: вниз Math.floor, вверх Math.ceil,
+ // до ближайшего Math.round
+
+console.log();
